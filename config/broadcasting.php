c340dfecb9d102762d10a428b93b923b5c455446
rasool_sh <rasool.sh344@gmail.com>
2023-10-16 07:08:21 +0330

    change pusher config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,7 +36,9 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER','ap1'),
+                'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
+                'host' => '127.0.0.1',
+                'port' => 6001,
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
