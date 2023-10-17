133f0c9a9e8de170bc063a0575da8c3bb6fa7360
rasool_sh <rasool.sh344@gmail.com>
2023-10-17 07:53:10 +0330

    add ssl cert

--- config/websockets.php
+++ config/websockets.php
@@ -116,14 +116,14 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', null),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', base_path().'/ssl/ssl.cert'),
         // '/etc/ssl/virtualmin/1695445185337285/ssl.cert'
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', null),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', base_path().'/ssl/ssl.key'),
         // '/etc/ssl/virtualmin/1695445185337285/ssl.key'
 
         /*
