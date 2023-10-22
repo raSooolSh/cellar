9b4990687c11cb2f84d81b40d88c9cfbd7c2418c
rasool_sh <rasool.sh344@gmail.com>
2023-10-22 23:36:12 +0330

    add ssl pem\

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -39,14 +39,15 @@ return [
                 'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
                 'host' => '127.0.0.1',
                 'port' => 6001,
+                'encrypted' => true,
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
-            'curl_options' => [
-                CURLOPT_SSL_VERIFYHOST => 0,
-                CURLOPT_SSL_VERIFYPEER => 0,
-                // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
-            ],
+            // 'curl_options' => [
+            //     CURLOPT_SSL_VERIFYHOST => 0,
+            //     CURLOPT_SSL_VERIFYPEER => 0,
+            //     // Guzzle client options: https://docs.guzzlephp.org/en/stable/request-options.html
+            // ],
         ],
 
         'ably' => [
