695f8e1d9b397051372ce1ba299e397503051b85
rasool_sh <rasool.sh344@gmail.com>
2023-10-24 06:46:41 +0330

    change websocket config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -36,10 +36,10 @@ return [
             'secret' => env('PUSHER_APP_SECRET'),
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
-                'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
+                // 'cluster' => env('PUSHER_APP_CLUSTER', 'ap1'),
                 'host' => env('PUSHER_HOST','127.0.0.1'),
                 'port' => env('PUSHER_PORT','6001'),
-                // 'encrypted' => true,
+                'encrypted' => true,
                 'scheme' => env('PUSHER_SCHEME', 'https'),
                 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
