a5d599f7e01955cb1343998defaace35faec4185
rasool_sh <rasool.sh344@gmail.com>
2023-11-04 07:04:57 +0330

    change websocket config

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -41,7 +41,7 @@ return [
                 'host' => env('PUSHER_HOST','127.0.0.1'),
                 'port' => env('PUSHER_PORT','6001'),
                 'scheme' => env('PUSHER_SCHEME', 'https'),
-                // 'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
+                'useTLS' => env('PUSHER_SCHEME', 'https') === 'https',
             ],
             'curl_options' => [
                 CURLOPT_SSL_VERIFYHOST => 0,
